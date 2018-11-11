165ebd8f8e8392bf0c367fa2382ae2ce819bb746
Wiktor Ślęczka <wikii122@gmail.com>
2018-11-11 17:12:26 +0100

    Compile, serve and watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
-function compileScss(cb) {
+function compileScss() {
   return gulp.src("static/styles/main.scss")
     .pipe(sass())
     .pipe(gulp.dest('static/styles'));
 }
 
-function serve(cb) {
-  cb();
+function watchScss() {
+  return gulp.watch("static/**/*.scss", compileScss);
 }
 
 function refreshBrowser() {
@@ -24,4 +24,4 @@ const build = compileScss;
 
 exports.default = build;
 exports.build = build;
-exports.dev= gulp.series(build, refreshBrowser);
+exports.dev = gulp.series(build, gulp.parallel(watchScss, refreshBrowser));
