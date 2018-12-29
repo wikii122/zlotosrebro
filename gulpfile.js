d78ff9ab559ccbc0e90e617ef5d65af32c560fb0
Wiktor Ślęczka <wikii122@gmail.com>
2018-12-29 11:28:45 +0100

    Site now compiles to dist/

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,52 @@
+const del = require('del');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
+function clean() {
+  return del('dist/**/*', {force:true});
+}
+
+function fonts() {
+  return gulp.src('static/fonts/**')
+    .pipe(gulp.dest('./dist/fonts'));
+}
+
+function images() {
+  return gulp.src('static/img/**')
+    .pipe(gulp.dest('./dist/img'));
+}
+
+function html() {
+  return gulp.src(['./static/*.html', 'static/**/*.js'])
+    .pipe(gulp.dest('./dist'));
+}
+
 function compileScss() {
-  return gulp.src("static/styles/main.scss")
+  return gulp.src('./static/styles/main.scss')
     .pipe(sass())
-    .pipe(gulp.dest('static/styles'));
+    .pipe(gulp.dest('./dist/styles'));
+}
+
+function watchStaticFiles() {
+  return gulp.watch(['./static/*.html', 'static/fonts', 'static/img', 'static/lib/**/*.js'], staticFiles)
 }
 
 function watchScss() {
-  return gulp.watch("static/**/*.scss", compileScss);
+  return gulp.watch('./dist/**/*.scss', compileScss);
 }
 
 function refreshBrowser() {
   browserSync.init({
     server: {
-        baseDir: "./static"
+        baseDir: './dist'
     }
   });
 }
-
-const build = compileScss;
+const staticFiles = gulp.parallel(html, fonts, images);
+const build = gulp.parallel(compileScss, staticFiles);
 
 exports.default = build;
 exports.build = build;
-exports.dev = gulp.series(build, gulp.parallel(watchScss, refreshBrowser));
+exports.dev = gulp.series(build, gulp.parallel(watchScss, watchStaticFiles, refreshBrowser));
+exports.clean = clean;
