6df1da8b733d41a54b134ac6b683e3881275653a
Wiktor Ślęczka <wikii122@gmail.com>
2018-12-30 13:59:38 +0100

    Moved cookie warning to bottom

--- static/lib/main.js
+++ static/lib/main.js
@@ -4,19 +4,18 @@ void function() {
         const str = '; '+ document.cookie +';';
         const index = str.indexOf('; '+ escape(name) +'=');
 
-        var warning = document.getElementById("cookie-warning");
+        const warning = document.getElementById("cookie-warning");
 
         function hideWarning() {
+            const expire = new Date();
+            expire.setTime((new Date()).getTime() + 3600000*24*365);
+            document.cookie = name + "=1;expires=" + expire;
             warning.classList.add("is-invisible");
         };
 
         if (!(index === -1 && navigator.cookieEnabled)) {
             hideWarning();
         }
-
-        var expire = new Date();
-        expire.setTime((new Date()).getTime() + 3600000*24*365);
-        document.cookie = name + "=1;expires=" + expire;
 
         warning.onclick = hideWarning;
     }
